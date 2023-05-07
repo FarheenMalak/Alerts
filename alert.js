9e33ff92b3377daf89416875087b16e0d95fe76d
FarheenMalak <115576023+FarheenMalak@users.noreply.github.com>
2023-05-07 19:32:59 +0500

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,16 @@
+// #1 ALERTS
+
+console.log("#1 ALERT");
+alert("Hello There!");
+
+alert("Error! Please Enter a valid Password");
+
+alert("Welcome to JS Land \nHappy Coding ");
+
+alert("Hello...I can run JS through My Web Browser’s Console ");
+
+// alert(5 + 5);
+
+alert("My Name is Farheen Malak");
+
+alert("“__________ஜ۩۞۩ஜ__________”");
